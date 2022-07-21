f318827ebebce6cb01ff6fbff2e86ad2cf144ced
DenisVlasiuk <fcdenisvlasiuk@gmail.com>
2022-07-21 18:26:37 +0300

    changes added

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,14 +1,19 @@
 public class Main {
 
+    // 1 - вариант с использыванием метода
     public static void main(String[] args) {
 // ООП: Создание классов и объектов
         Transport bmw = new Transport();
-        bmw.speed = 250.5f;
-        bmw.weight = 2500;
-        bmw.color = "White";
-        bmw.coordinate = new byte[] {0, 0, 0,};
-        bmw.wheelSize = 18;
+        bmw.setValues(250.5f, 2500, "White", new byte[] {48 , 72, 60}, 18);
+        String result1 = bmw.getValues();
+        System.out.println(result1);
+//        bmw.speed = 250.5f;
+//        bmw.weight = 2500;
+//        bmw.color = "White";
+//        bmw.coordinate = new byte[] {0, 0, 0,};
+//        bmw.wheelSize = 18;
 
+        // 2 - вариант без использывания метода
         Transport truck = new Transport();
         truck.speed = 180.4f;
         truck.weight = 5600;
@@ -16,7 +21,9 @@ public class Main {
         truck.coordinate = new byte[] {48 , 72, 60};
         truck.wheelSize = 25;
 
-        System.out.println("Truck speed: " + truck.speed + ". Truck weight: " + truck.weight + ". Bmw color: " + bmw.color + ".");
+        String result2 = truck.getValues();
+        System.out.println(result2);
+//        System.out.println(". Truck speed: " + truck.speed + "\n. Truck weight: " + truck.weight + "\n. Bmw color: " + bmw.color + "\n.");
 
     }
 }
--- src/test/java/Transport.java
+++ src/test/java/Transport.java
@@ -6,7 +6,26 @@ public class Transport {
     public byte[] coordinate;
     public int  wheelSize;
 
+    // setValues - Установить значения!
+    public void setValues(float speed2, int weight2, String color2, byte[] coordinate2, int wheelSize2){
+        speed = speed2;
+        weight = weight2;
+        color = color2;
+        coordinate = coordinate2;
+        wheelSize = wheelSize2;
 
+    }
+    // getValues - Получить значения!
+    public String getValues() {
+        String info = "Object speed: " + speed + ". Weight: " + weight + ". Color: " + color +
+                ". WheelSize: " + wheelSize + ".\n";
+        String infoCoordinates  = "Coordinates\n";
+
+        for(int i = 0; i < coordinate.length; i++)
+            infoCoordinates += coordinate[i] + "\n";
+
+        return info + infoCoordinates;
+    }
 
 }
 
