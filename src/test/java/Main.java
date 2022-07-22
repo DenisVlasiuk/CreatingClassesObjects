cd7ff8477396a2b5f972499aff5201707623cd7e
DenisVlasiuk <fcdenisvlasiuk@gmail.com>
2022-07-22 14:34:30 +0300

    changes added - new constructor

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,28 +1,22 @@
 public class Main {
 
-    // 1 - вариант с использыванием метода
     public static void main(String[] args) {
 // ООП: Создание классов и объектов
-        Transport bmw = new Transport();
-        bmw.setValues(250.5f, 2500, "White", new byte[] {48 , 72, 60}, 18);
-        String result1 = bmw.getValues();
-        System.out.println(result1);
-//        bmw.speed = 250.5f;
-//        bmw.weight = 2500;
-//        bmw.color = "White";
-//        bmw.coordinate = new byte[] {0, 0, 0,};
-//        bmw.wheelSize = 18;
+        Transport bmw = new Transport(250.5f, 2500, "White", new byte[] {48 , 72, 60}, 18);
+        bmw.setValues(258, 1500, "red", new byte[] {0, 21}, 21);
+        System.out.println(bmw.getValues());
 
         // 2 - вариант без использывания метода
-        Transport truck = new Transport();
-        truck.speed = 180.4f;
-        truck.weight = 5600;
-        truck.color = "Yelow";
-        truck.coordinate = new byte[] {48 , 72, 60};
-        truck.wheelSize = 25;
 
-        String result2 = truck.getValues();
-        System.out.println(result2);
+        Transport truck = new Transport(2500, "Yellow");
+//        truck.speed = 180.4f;
+//        truck.weight = 5600;
+//        truck.color = "Yellow";
+//        truck.coordinate = new byte[] {48 , 72, 60};
+//        truck.wheelSize = 25;
+//
+//        String result2 = truck.getValues();
+//        System.out.println(result2);
 //        System.out.println(". Truck speed: " + truck.speed + "\n. Truck weight: " + truck.weight + "\n. Bmw color: " + bmw.color + "\n.");
 
     }
--- src/test/java/Transport.java
+++ src/test/java/Transport.java
@@ -6,18 +6,30 @@ public class Transport {
     public byte[] coordinate;
     public int  wheelSize;
 
-    // setValues - Установить значения!
-    public void setValues(float speed2, int weight2, String color2, byte[] coordinate2, int wheelSize2){
-        speed = speed2;
+    public Transport(float speed2, int weight2, String color2, byte[] coordinate2, int wheelSize2){
+        System.out.println("Den4ik");
+        setValues(speed2, weight2, color2, coordinate2, wheelSize2);
+        System.out.println(getValues());
+    }
+
+    public Transport(int weight2, String color2){
         weight = weight2;
         color = color2;
-        coordinate = coordinate2;
-        wheelSize = wheelSize2;
+        System.out.println(this.getValues());
+    }
+
+    // setValues - Установить значения!
+    public void setValues(float speed, int weight, String color, byte[] coordinate, int wheelSize){
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
+        this.wheelSize = wheelSize;
 
     }
     // getValues - Получить значения!
     public String getValues() {
-        String info = "Object speed: " + speed + ". Weight: " + weight + ". Color: " + color +
+        String info = "Object speed: " + this.speed + ". Weight: " + this.weight + ". Color: " + this.color +
                 ". WheelSize: " + wheelSize + ".\n";
         String infoCoordinates  = "Coordinates\n";
 
